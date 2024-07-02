28f901d0ab561731c611444cb5bd64d0b827ebe8
mechm <maria.chuico@unl.edu.ec>
2024-07-02 04:29:05 -0500

    Clase ArbolIBB

---
+++ ArbolBinario/src/arbolBB/ArbolBB.java
@@ -0,0 +1,165 @@
+package arbolBB;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import javax.swing.JPanel;
+
+public class ArbolBB {
+
+    private Nodo raiz;
+    int num_nodos;
+    int alt;
+
+    public ArbolBB() {
+        raiz = null;
+    }
+    
+    public boolean agregar(int dato) {
+        Nodo nuevo = new Nodo(dato, null, null);
+        insertar(nuevo, raiz);
+        return true;
+    }
+    
+    //Metodo para insertar un dato en el arbol (no acepta repetidos)
+    public void insertar(Nodo nuevo, Nodo pivote) {
+        if (this.raiz == null) {
+            raiz = nuevo;
+        } else {
+            if (nuevo.getDato() <= pivote.getDato()) {
+                if (pivote.getIzq() == null) {
+                    pivote.setIzq(nuevo);
+                } else {
+                    insertar(nuevo, pivote.getIzq());
+                }
+            } else {
+                if (pivote.getDer() == null) {
+                    pivote.setDer(nuevo);
+                } else {
+                    insertar(nuevo, pivote.getDer());
+                }
+            }
+        }
+
+    }
+
+    public Nodo getRaiz() {
+        return raiz;
+    }
+
+    public void setRaiz(Nodo raiz) {
+        this.raiz = raiz;
+    }
+
+    //Recorrido preorden, recibe el nodo a empezar (raiz) y una linkedlist para ir guardando el recorrido
+    public LinkedList preOrden() {
+        LinkedList rec = new LinkedList();
+        preorden(raiz, rec);
+        return rec;
+    }
+    
+    public void preorden(Nodo aux, LinkedList recorrido) {
+        if (aux != null) {
+            recorrido.add(aux.getDato());
+            preorden(aux.getIzq(), recorrido);
+            preorden(aux.getDer(), recorrido);
+        }
+    }
+
+    //Recorrido inorden
+    public LinkedList inOrden() {
+        LinkedList rec = new LinkedList();
+        inorden(raiz, rec);
+        return rec;
+    }
+    
+    public void inorden(Nodo aux, LinkedList recorrido) {
+        if (aux != null) {
+            inorden(aux.getIzq(), recorrido);
+            recorrido.add(aux.getDato());
+            inorden(aux.getDer(), recorrido);
+        }
+    }
+
+    //Recorrido postorden
+    public LinkedList postOrden() {
+        LinkedList rec = new LinkedList();
+        postorden(raiz, rec);
+        return rec;
+    }
+    public void postorden(Nodo aux, LinkedList recorrido) {
+        if (aux != null) {
+            postorden(aux.getIzq(), recorrido);
+            postorden(aux.getDer(), recorrido);
+            recorrido.add(aux.getDato());
+        }
+    }
+
+    //Metodo para comprobar si hay un nodo en el arbol
+    public boolean existe(int dato) {
+        Nodo aux = raiz;
+        while (aux != null) {
+            if (dato == aux.getDato()) {
+                return true;
+            } else if (dato > aux.getDato()) {
+                aux = aux.getDer();
+            } else {
+                aux = aux.getIzq();
+            }
+        }
+        return false;
+    }
+
+    private void altura(Nodo aux, int nivel) {
+        if (aux != null) {
+            altura(aux.getIzq(), nivel + 1);
+            alt = nivel;
+            altura(aux.getDer(), nivel + 1);
+        }
+    }
+
+    //Devuelve altura del arbol
+    public int getAltura() {
+        altura(raiz, 1);
+        return alt;
+    }
+    
+     public JPanel getdibujo() {
+        return new ArbolExpresionGrafico(this);
+    }
+
+    public void eliminar(int dato) {
+        raiz = eliminarRec(raiz, dato);
+    }
+    private Nodo eliminarRec(Nodo nodo, int dato) {
+        if (nodo == null) {
+            return nodo;
+        }
+
+        if (dato < nodo.getDato()) {
+            nodo.setIzq(eliminarRec(nodo.getIzq(), dato));
+        } else if (dato > nodo.getDato()) {
+            nodo.setDer(eliminarRec(nodo.getDer(), dato));
+        } else {
+            // Nodo con solo un hijo o sin hijos
+            if (nodo.getIzq() == null) {
+                return nodo.getDer();
+            } else if (nodo.getDer() == null) {
+                return nodo.getIzq();
+            }
+
+            nodo.setDato(minValue(nodo.getDer()));
+            nodo.setDer(eliminarRec(nodo.getDer(), nodo.getDato()));
+        }
+
+        return nodo;
+    }
+
+    private int minValue(Nodo nodo) {
+        int minv = nodo.getDato();
+        while (nodo.getIzq() != null) {
+            minv = nodo.getIzq().getDato();
+            nodo = nodo.getIzq();
+        }
+        return minv;
+    }
+}
